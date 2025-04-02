b5a04649bc85a6e4186e12c64ec83ec4e317b594
Krasimir Ivanov <krasimir.ivanov@optimal-industries.com>
2025-04-02 18:33:56 +0300

    Changing pass

--- src/utils/passCheck.ts
+++ src/utils/passCheck.ts
@@ -1,4 +1,4 @@
-const HASH = 'e751efb8b652817fa0c47700ad07167bf7140bb7f137fdfae9ccf233baa4e69c';
+const HASH = '9af15b336e6a9619928537df30b2e6a2376569fcf9d7e773eccede65606529a0';
 
 export async function verifyPassword(inputPassword: string) {
   const encoder = new TextEncoder();
@@ -9,4 +9,12 @@ export async function verifyPassword(inputPassword: string) {
     .join('');
 
   return hash === HASH;
+}
+export async function generateHash(input: string): Promise<string> {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(input);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
 }
